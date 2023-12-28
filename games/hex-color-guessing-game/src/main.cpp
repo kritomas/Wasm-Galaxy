b902190f79608359741aed536a6cfce00935693e
kritomas <Unknown>
2023-12-28 17:27:12 +0100

    Exported functions test.

--- games/hex-color-guessing-game/src/main.cpp
+++ games/hex-color-guessing-game/src/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+int sqr(int num)
+{
+	return num * num;
+}
+
+extern "C"
+{
+int csqr(int num)
+{
+	return sqr(num);
+}
+}
+
 int main(int argc, char* argv[])
 {
 	std::cout << "Hello World!" << std::endl;
